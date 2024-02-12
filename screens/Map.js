da932bbd05951a45ebd277998043492d4bced0ea
Laura <127945558+lavia1@users.noreply.github.com>
2024-02-12 16:11:52 +0200

    Update Map.js

--- screens/Map.js
+++ screens/Map.js
@@ -1,45 +1,39 @@
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import * as Location from 'expo-location';
-import { useEffect } from "react";
 
-export default function Map(props, {location, setLocation}){
+export default function Map(props) {
+  const [marker, setMarker] = useState(null);
 
-    const getUserPosition = async () => {
-        let{status} = await Location.requestForegroundPermissionsAsync()
+  const showMarker = (e) => {
+    const coords = e.nativeEvent.coordinate;
+    setMarker(coords);
+  };
 
-        try {
-            if (status !== 'granted') {
-                console.log('Geolocation failed');
-                return;
-            }
-            const position = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
-            setLocation({...location, "latitude": position.coords.latitude,"longitude": position.coords.longitude})
-        } catch (error) {
-            console.log(error)
-        }
-    };
-
- /*   useEffect (() => {
-        (async() => {
-            getUserPosition()
-        })()
-    }, []);
-*/
-
-    return (
-        <>
-        <MapView
-            style={styles.map}
-            region={location}
+  return (
+    <MapView
+      style={styles.map}
+      region={props.location}
+      mapType='satellite'
+      onLongPress={showMarker}
+    >
+      {marker && (
+        <Marker
+          title="My marker"
+          coordinate={{
+            latitude: marker.latitude,
+            longitude: marker.longitude,
+          }}
         />
-        </>
-    );
+      )}
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
-    map: {
-        height: '100%',
-        width: '100%'
-    }
+  map: {
+    height: '100%',
+    width: '100%',
+  },
 });
